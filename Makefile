51ea049785ad3bdac8263c9ce50632a4eb8a8774
Mikalai Kardash <mikalai.kardash@gmail.com>
2026-07-13 18:51:37 +0300

    (added): stats lib, use it in false sharing sample.

---
+++ Makefile
@@ -0,0 +1,22 @@
+export ROOT_DIR := $(shell pwd)
+export SDK_LIB_DIR := $(ROOT_DIR)/sdk/c
+
+
+LIB_STATS    := sdk/c/stats
+LIB_STATS_A := $(LIB_STATS)/libsdk_stats.a
+
+APP_MEMORY_FALSE_SHARING_DIR := memory/false_sharing/c
+APP_MEMORY_FALSE_SHARING_APP := $(APP_MEMORY_FALSE_SHARING_DIR)/fs
+
+all: $(LIB_STATS_A) $(APP_MEMORY_FALSE_SHARING_APP)
+
+$(LIB_STATS_A):
+	$(MAKE) -C $(LIB_STATS)
+
+$(APP_MEMORY_FALSE_SHARING_APP): $(LIB_STATS_A)
+	$(MAKE) -C $(APP_MEMORY_FALSE_SHARING_DIR)
+
+clean:
+	@for dir in $(LIB_STATS) $(APP_MEMORY_FALSE_SHARING_DIR); do \
+		$(MAKE) -C $$dir clean; \
+	done
---
+++ sdk/c/stats/stats.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include "stats.h"
+
+void calculate_stats(float *results, int length, struct stats *st) {
+    float min = 10000000000.0;
+    float max = 0.0;
+    float sum = 0.0;
+
+    for (int i = 0; i < length; i++) {
+        min = (min > results[i]) ? results[i] : min;
+        max = (max < results[i]) ? results[i] : max;
+        sum += results[i];
+    }
+
+    float avg = sum / (float) length;
+    float std = 0.0;
+    for (int i = 0; i < length; i++) {
+        float diff = results[i] - avg;
+        std += diff * diff;
+    }
+    
+    float deviation = sqrtf(std / length);
+
+    float p95min = avg - deviation*2;
+    p95min = (p95min < min) ? min : p95min;
+
+    float p95max = avg + deviation*2;
+    p95max = (p95max > max) ? max : p95max;
+
+    st->avg = avg;
+    st->min = min;
+    st->max = max;
+    st->deviation = deviation;
+    st->p95max = p95max;
+    st->p95min = p95min;
+}
